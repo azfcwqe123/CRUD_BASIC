f2f6ebd510f23cbff0e8cf3685fc9d8a4449b3a0
azfcwqe123 <azfcwqe123@gmail.com>
2025-03-01 22:12:41 +0900

    2025-03-01

--- src/main/java/com/example/CRUD_BASIC/controller/BoardController.java
+++ src/main/java/com/example/CRUD_BASIC/controller/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -41,6 +42,7 @@ public class BoardController {
         // 2. DB에 엔티티 담기
         postRepository.save(post);
         log.info(post.toString());
+        // 리다이렉트
         return "redirect:/home";
     }
 
@@ -52,6 +54,40 @@ public class BoardController {
         // 엔티티를 모델에 담는다.
         model.addAttribute("Post", post);
         return "board/show";
+    }
+
+    // Update (게시글 수정)
+    @GetMapping("/home/{id}/edit")
+    public String editWriting(@PathVariable Long id, Model model) {
+        Post post = postRepository.findById(id).orElse(null);
+        model.addAttribute("Post", post);
+        return "board/edit";
+    }
+
+    // Update (게시글 수정)
+    @PostMapping("/home/update")
+    public String updateWriting(PostForm form) {
+        // 1. DTO -> 엔티티
+        Post postEntity = form.toEntity();
+        // 2. 기존에 있던 게시글을 수정.
+        Post check = postRepository.findById(postEntity.getId()).orElse(null);
+        if(check != null) postRepository.save(postEntity);
+
+        // 리다이렉트
+        return "redirect:/home";
+    }
+
+    // Delete (게시글 삭제)
+    @GetMapping("/home/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        Post target = postRepository.findById(id).orElse(null);
+
+        if(target != null) {
+            postRepository.delete(target);
+            // 리다이렉트 시점에 한 번만 사용할 데이터
+            rttr.addFlashAttribute("msg", "게시글이 삭제되었습니다.");
+        }
+        return "redirect:/home";
     }
 
 }
